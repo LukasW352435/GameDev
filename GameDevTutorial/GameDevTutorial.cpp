8dbdfdd531bd4cf6582e972dbed81c7e0bff8d0d
luw5008 <37829455+LukasW352435@users.noreply.github.com>
2021-12-02 17:32:24 +0100

    Add project files.

---
+++ GameDevTutorial/GameDevTutorial.cpp
@@ -0,0 +1,12 @@
+﻿// GameDevTutorial.cpp : Defines the entry point for the application.
+//
+
+#include "GameDevTutorial.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
